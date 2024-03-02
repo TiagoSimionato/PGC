7cbc9f79c61ddd41888ef1293157440cec1d738e
Tiago Simionato <tiago.simionato5@gmail.com>
2024-03-02 19:27:39 -0300

    feat: appium parser

---
+++ example.py
@@ -0,0 +1,41 @@
+import argparse
+import unittest
+
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-ap', '--appiumPort', type=int, default=4723, help='Port to appium server running on localhost')
+args = vars(ap.parse_args())
+
+APPIUM_SERVER_URL =  f'http://192.168.15.35:{args['appiumPort']}'
+
+capabilities = dict(
+  platformName='Android',
+  automationName='uiautomator2',
+  deviceName='avd',
+  appPackage='com.google.android.calculator',
+  appActivity='com.android.calculator2.Calculator',
+  language='en',
+  locale='US'
+)
+capabilities_options = UiAutomator2Options().load_capabilities(capabilities)
+
+class TestAppium(unittest.TestCase):
+  def setUp(self) -> None:
+    self.driver = webdriver.Remote(command_executor=APPIUM_SERVER_URL, options=capabilities_options)
+
+  def tearDown(self) -> None:
+    if self.driver:
+      self.driver.quit()
+
+  def test_find_battery(self) -> None:
+    el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+    el.click()
+
+  def test_dump(self) -> None:
+    print(self.driver.page_source)
+
+if __name__ == '__main__':
+  unittest.main()
